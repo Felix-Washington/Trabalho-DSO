a120686d9bd78b576175970095f8d21933132414
Francielle Lopes <franciellelps@gmail.com>
2020-10-28 23:12:13 +0000

    Início da implementação do ControladorCarrinho

--- controlador/controlador_carrinho.py
+++ controlador/controlador_carrinho.py
@@ -1,15 +1,37 @@
 from controlador.abstract_controlador import AbstractControlador
 
+from tela_carrinho import TelaCarrinho
+
 class ControladorCarrinho(AbstractControlador):
 
   def __init__(self):
+    self.__tela_carrinho = TelaCarrinho(self)
+    self.__carrinhos = []
+
+  def iniciar (self):
     pass
 
-  def adicionar(self):
+  def listar(self):
     pass
 
-  def remover(self):
+  def add (self):
     pass
 
-  def atualizar(self):
+  def remover (self):
     pass
+
+  def limpar_carrinho(self):
+    pass
+    
+  def valor_total(self):
+    pass
+
+  def finalizar_tela(self):
+    pass
+
+  def abrir_tela_inicial(self):
+    pass
+
+
+
+  
