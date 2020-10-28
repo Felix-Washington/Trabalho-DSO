d29ac4d733b2342a213b24505d81ed7a0534ee8b
Francielle Lopes <franciellelps@gmail.com>
2020-10-28 23:30:38 +0000

    Ajustes no ControladorPrincipal

--- controlador/controlador_principal.py
+++ controlador/controlador_principal.py
@@ -3,49 +3,35 @@ from controlador.controlador_cliente import ControladorCliente
 from controlador.controlador_funcionario import ControladorFuncionario
 from controlador.controlador_menu import ControladorMenu
 from controlador.controlador_produto import ControladorProduto
-
-from tela.tela_menu import TelaMenu
-
+from tela.tela_principal import TelaPrincipal
 
 class ControladorPrincipal():
   def __init__(self):
-    self.__controlador_carrinho = ControladorCarrinho()
     self.__controlador_cliente = ControladorCliente()
     self.__controlador_funcionario = ControladorFuncionario()
-    self.__controlador_menu = ControladorMenu()
-    self.__controlador_produto = ControladorProduto()
+    self.__tela_principal = TelaPrincipal(self)
 
-    self.__tela_menu = TelaMenu()
+ 
+  def iniciar_sistema(self):
+    self.abrir_tela_inicial()
 
+  def mostrar_tela_funcionario(self):
+    self.__controlador_funcionario.abrir_tela_inicial()
 
-  def inicia_sistema(self):
-    self.abre_tela()
+  def mostrar_tela_cliente(self):
+    self.__controlador_cliente.abrir_tela_inicial()
 
-
-  def mostra_tela_funcionario(self):
-    self.__controlador_funcionario.abre_tela()
-    pass
-
-
-  def mostra_tela_cliente(self):
-    self.__controlador_cliente.abre_tela()
-
-
-  def fecha_sistema(self):
+  def finalizar_tela(self):
     exit(0)
 
-
-  def abre_tela(self):
+  def abrir_tela_inicial(self):
     lista_opcoes = {
     1: self.mostra_tela_cliente,
     2: self.mostra_tela_funcionario, 
     0: self.fecha_sistema}
 
     while True:
-  
       opcao_escolhida = self.__tela_menu.mostra_opcoes()
-
       funcao_escolhida = lista_opcoes[opcao_escolhida]
-
       funcao_escolhida()
     
