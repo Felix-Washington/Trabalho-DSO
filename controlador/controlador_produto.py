197a1336d0616bf72bf070bffd9e1f2f47a5b552
Francielle Lopes <franciellelps@gmail.com>
2020-10-28 23:32:47 +0000

    Ajustes no ControladorProduto

--- controlador/controlador_produto.py
+++ controlador/controlador_produto.py
@@ -1,27 +1,41 @@
 from controlador.abstract_controlador import AbstractControlador
+from tela.tela_produto import TelaProduto
 
 class ControladorProduto(AbstractControlador):
   def __init__(self):
+    self.__tela_produto = TelaProduto(self)
     self.__produtos = []
+  
+  def iniciar (self):
     pass
 
-  def adicionar(self):
+  def add (self):
     pass
 
-
-  def remover(self):
+  def remover (self):
     pass
 
-
-  def atualizar(self):
+  def atualizar (self):
     pass
 
+  def listar(self):
+    pass
 
+  def abrir_tela_inicial(self):
+    pass
+    
+  def finalizar_tela(self):
+    pass
+
+  def relatorio_estoque(self):
+    pass
+
+  def atualizar_estoque(self):
+    pass
+    
+  def imprimir_relatorio(self):
+    pass
+    
   @property
   def produtos(self):
     return self.__produtos
-
-
-  @produtos.setter
-  def produtos(self, produtos):
-    self.__produtos = produtos
